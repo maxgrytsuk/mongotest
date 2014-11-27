9823aaaa74c8bdecdf6719040864990200367b82
Max Grytsuk <max@appto.us>
2014-11-27 15:58:49 +0200

    move getting started code to separate func

--- app.js
+++ app.js
@@ -56,39 +56,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/kittens');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-    var kittySchema = mongoose.Schema({
-        name: String
-    })
-    kittySchema.methods.speak = function () {
-        var greeting = this.name
-            ? "Meow name is " + this.name
-            : "I don't have a name"
-        console.log(greeting);
-    }
-    var Kitten = mongoose.model('Kitten', kittySchema)
-    var silence = new Kitten({ name: 'Silence' })
-    silence.save(function (err, silence) {
-        if (err) return console.error(err);
-        silence.speak();
-    });
-    var fluffy = new Kitten({ name: 'Fluffy' });
-    fluffy.save(function (err, fluffy) {
-        if (err) return console.error(err);
-        fluffy.speak();
-    });
-    var callback = function (err, kittens) {
-        if (err) return console.error(err);
-        console.log(kittens)
-    };
-    Kitten.find(callback)
-    Kitten.find({ name: /^Fluff/ }, callback)
-});
-
-
 module.exports = app;
